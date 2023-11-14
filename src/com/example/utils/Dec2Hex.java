5725f5e08174d90aa39b13a3f6aba0d5aa424ddf
russsseeelll <russellmci@hotmail.co.uk>
2023-11-14 15:58:04 +0000

    original code demonstration

--- src/com/example/utils/Dec2Hex.java
+++ src/com/example/utils/Dec2Hex.java
@@ -1,44 +1,28 @@
-package com.example.utils;
+import java.util.Scanner;
 
-import java.util.logging.Logger;
-import java.util.logging.Level;
+class Dec2Hex
+{
 
-public class Dec2Hex {
+    public static int Arg1;
 
-    private static final Logger LOGGER = Logger.getLogger(Dec2Hex.class.getName());
+    public static void main(String args[])
+    {
+        Arg1 = Integer.parseInt(args[0]);
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem, num;
+        num = Arg1;
+        String hexadecimal="";
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-    public static void main(String[] args) {
-        // Check if an argument is provided
-        if (args.length == 0) {
-            LOGGER.log(Level.SEVERE, "Error: No input argument provided.");
-            return; // Exit the program
+        while(num != 0)
+        {
+            rem=num%16;
+            hexadecimal= ch[rem] + hexadecimal;
+            num= num/16;
         }
 
-        int arg1;
-        try {
-            // Attempt to parse the argument as an integer
-            arg1 = Integer.parseInt(args[0]);
-        } catch (NumberFormatException e) {
-            // If parsing fails, log an error and exit
-            LOGGER.log(Level.SEVERE, "Error: Input must be an integer.");
-            return; // Exit the program
-        }
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
-        // Continue with the conversion if input is valid
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem;
-        int num = arg1;
-        StringBuilder hexadecimal = new StringBuilder();
-
-     	LOGGER.log(Level.INFO, "Hexadecimal representation is: {0}", hexadecimal.toString());
-
-        while (num != 0) {
-            rem = num % 16;
-            hexadecimal.insert(0, ch[rem]);
-            num = num / 16;
-        }
-
-        LOGGER.info("Hexadecimal representation is: " + hexadecimal);
     }
 }
 
